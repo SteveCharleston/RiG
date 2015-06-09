9505c8f83e273f671eb3cd9b4a5d283155ef5019
SteveCharleston <schalhorn@gmail.com>
2015-06-09 18:54:37 +0200

    Erste Arbeiten am Tags und Tage Fragment

---
+++ app/src/main/java/de/charlestons_inn/rig/TagsAndDays.java
@@ -0,0 +1,30 @@
+package de.charlestons_inn.rig;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TagsAndDays extends Fragment {
+
+
+    public TagsAndDays() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tags_and_days, container, false);
+    }
+
+
+}
