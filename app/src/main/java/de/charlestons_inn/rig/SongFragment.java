e51eec2c89402a2d1d3a49f7dae9591ce4d42ea3
SteveCharleston <schalhorn@gmail.com>
2015-06-15 18:50:15 +0200

    Musicplayer adapter prinzipiell mit werten befüllbar

---
+++ app/src/main/java/de/charlestons_inn/rig/SongFragment.java
@@ -0,0 +1,66 @@
+package de.charlestons_inn.rig;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.ListFragment;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+import rigAPI.RigBand;
+import rigAPI.RigDBAccess;
+import rigAPI.Song;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnFragmentInteractionListener}
+ * interface.
+ */
+public class SongFragment extends ListFragment {
+    private RigDBAccess rig;
+    private RigBand currentBand;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public SongFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle bundle = this.getArguments();
+        String apiKey = bundle.getString("apiKey");
+        rig = (RigDBAccess) bundle.getSerializable("rig");
+        currentBand = (RigBand) bundle.getSerializable("currentBand");
+
+        setListAdapter(
+                new ArrayAdapter<Song>(
+                        getActivity(),
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1,
+                        currentBand.getSongs()));
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+    }
+}
