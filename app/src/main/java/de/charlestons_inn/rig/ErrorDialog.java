9703f45a85fa21033a42bb9ee7fd0e43c10cd988
lennox <kyalo.lennox@hotmail.de>
2015-06-26 16:22:44 +0200

    Button hinzugefügt

--- app/src/main/java/de/charlestons_inn/rig/ErrorDialog.java
+++ app/src/main/java/de/charlestons_inn/rig/ErrorDialog.java
@@ -5,6 +5,7 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Lennox on 19/06/2015.
@@ -19,9 +20,20 @@ public class ErrorDialog extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
-        View view = inflater.inflate(R.layout.activity_pop_up_window, container);
+        View view = inflater.inflate(R.layout.error_dialog, container);
+        final Button button= (Button) view.findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener(){
+                                      public void onClick(View v){
+
+
+
+
+                                      }
+                                }
+        );
 
         return view;
     }
+
 }
 
