95c5d795846b56a9b5e2199b38f77d2b4c321002
lennox <kyalo.lennox@hotmail.de>
2015-07-06 06:54:44 +0200

    bandsuche

--- app/src/main/java/de/charlestons_inn/rig/Bandsuche.java
+++ app/src/main/java/de/charlestons_inn/rig/Bandsuche.java
@@ -1,8 +1,10 @@
 package de.charlestons_inn.rig;
 
+
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +13,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+
 
 import java.util.concurrent.ExecutionException;
 
@@ -28,10 +31,12 @@ public class Bandsuche extends ActionBarActivity {
         RigDBAccess rig=new RigDBAccess();
         RigBand band=null;
         String query="";
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+        Intent intent=getIntent();
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())) {
             query = intent.getStringExtra(SearchManager.QUERY);
+
         }
+
 
         try {
            String  apiKey = new AsyncAuthenticate(this, rig)
@@ -45,9 +50,7 @@ public class Bandsuche extends ActionBarActivity {
             e.printStackTrace();
         }
 
-
              String [] Namen= new String[1];
-
 
            try {
                 band =new AsyncGetBand(this,rig).execute(1).get();
@@ -56,17 +59,13 @@ public class Bandsuche extends ActionBarActivity {
             } catch (ExecutionException e) {
                 e.printStackTrace();
             }
-            if(band.getName().contains(query)){
+            if((band.getName().startsWith(query))){
                 Namen[0]= band.getName();
             }
         else{
                 Namen[0]= new String("");
 
             }
-
-
-
-
 
         ListAdapter Namen_adapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,Namen);
         ListView view_Namen= (ListView)findViewById(R.id.listView);
@@ -79,15 +78,15 @@ public class Bandsuche extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_bandsuche, menu);
-        // Get the SearchView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
 
         return true;
+
     }
 
     @Override
