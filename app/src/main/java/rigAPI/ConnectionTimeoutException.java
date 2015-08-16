ba3dc3fe1891f485be2870a5bf182ec201f5d827
lennox <kyalo.lennox@hotmail.de>
2015-08-16 20:47:42 +0200

    timeout

---
+++ app/src/main/java/rigAPI/ConnectionTimeoutException.java
@@ -0,0 +1,12 @@
+package rigAPI;
+
+/**
+ * Created by Lennox on 16/08/2015.
+ */
+public class ConnectionTimeoutException extends RiGException  {
+
+    ConnectionTimeoutException(Exception e) {
+        super(e);
+    }
+
+}
